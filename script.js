45ae0471e50a8967b149cff493aa48992a1237e7
yutu58 <165717@driestarleerling.nl>
2018-12-31 00:22:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,133 @@
+var comms = ["[R2 U\' : [S, R2]]", "[U\' M2 U : [M, U2]]", "[U\' M U\' : [M\', U2]]", "[U\' : [R\' E R, U2]]", "[U : [L\' E\' L, U2]]", "[U\' : [R E\' R\', U2]]", "[R\' U\' : [S, R2]]", "[U\' M U : [M, U2]]", "[U\' : [R E2 R\', U2]]", "[S U\' : [S, R2]]", "[U : [L\' E L, U2]]", "[U\' : [R E R\', U2]]", "[U : [L E\' L\', U2]]", "[U\' : [R\' E2 R, U2]]", "[U M\' U : [M\', U2]]", "[R U\' : [S, R2]]", "[U2, M\']", "[U\' : [S, R2]]", "[M, U2]", "[U : [S\', L2]]", "R2 U\' F B\' R2 F\' B U\' R2", "[M2 U : [M, U2]]", "[S\', L F\' L\']", "[U\', R\' E R]", "[U : [L\' E\' L, U]]", "[U\', R E\' R\']", "[E\' : [U\', R\' E R]]", "[M\', R U\' R\' U]", "[U\', R E2 R\']", "[U : [U, L\' E L]]", "[U\', R E R\']", "[U : [L E\' L\', U]]", "[M, R\' U\' R U]", "[U\', R\' E2 R]", "(U M U M\')2", "[M : [U M\' U\', R\']]", "[M U2 M, U]", "[R\' U R\' : [S, R2]]", "[M2, R U\' R\' U]", "U\' L\' U\' L U L U L U\' L\'", "[M2 : u M\' u2 M\' u]", "[M2 U\' : [M, U2]]", "[U\' : [R\' E R, U\']]", "[U, L\' E\' L]", "[U\' : [U\', R E\' R\']]", "[U, L\' E2 L]", "[M\', L\' U L U\']", "[E : [U, L E\' L\']]", "[S, R\' F R]", "[U, L\' E L]", "[U\' : [R E R\', U\']]", "[U, L E\' L\']", "[M, L U L\' U\']", "[E\' : [U, L\' E L]]", "(U\' M U\' M\')2", "[U, L E2 L\']", "[R\' F\' : [R S R\', F2]]", "U R U R\' U\' R\' U\' R\' U R", "D\' U R U R\' U\' R\' U\' R\' U R D", "[L U\' L : [S\', L2]]", "[U\' M U : [M\', U2]]", "[L F\' L\', S\']", "[S : [U\', R\' E R]]", "[F L\' : [E\', L2]]", "[S : [U\', R E\' R\']]", "[S\' U L\' : [E\', L2]]", "[L F : [L\' U L, E\']]", "[r\' F\' : [F\', r S r\']]", "[M U : [M\', U2]]", "[r\' U : [M\', U2]]", "[R\' F R : [S, R2]]", "[M R U : [M\', U2]]", "[L\' U\' L U, M\']", "[L\' u L\' : [E, L2]]", "[M\' : (U M\' U M)2]", "[S\' U L : [E, L2]]", "[L\' F : [E\', L2]]", "[L : [F\', L S L\']]", "[l\' : [M2, U\' L U]]", "[L F\' L\', S]", "[U : [R\' E R, U2]]", "[R\' E R, U\']", "[U\' : [U\', R\' E R]]", "[S\' : [R\' E R, U\']]", "[L, D M D\']", "[r U L : [S\', L2]]", "[E, R U\' R\']", "[F : [L\' U L, E\']]", "[M L\' U\' : [M\', U2]]", "[r U R\' : [E, R2]]", "[M\' U R\' : [E, R2]]", "[U E R\' : [S, R2]]", "[U\' L\' U, M\']", "[E\', L U L\']", "[l\' : (U M\' U M)2]", "[R\' : [R\' E R, U\']]", "[L2 F\' : [L2, E\']]", "[U\' E\' R : [E\', R2]]", "[M : [U\' L\' U, M2]]", "[U E L : [E\', L2]]", "[U\' : [L\' E\' L, U2]]", "[U : [U, L\' E\' L]]", "[L\' E\' L, U]", "[F L : [E\', L2]]", "[D M D\', L]", "[D M D\', L\']", "[D : [U\' R U, M\']]", "[U : [L F L\', S]]", "[S\' U\' R : [E, R2]]", "[L F\' L : [S\', L2]]", "[L F\' : [E, L2]]", "[M\' D : [M\', D2]]", "[M\' U\' L\' : [E\', L2]]", "[U\' L2 U, M\']", "[S\' U\' R\' : [E\', R2]]", "(M D M\' D)2", "[D : [U\' R\' U, M\']]", "[L F\' : [L2, E\']]", "[L\' F\' L, S]", "[U : [S\', L B\' L\']]", "[U : [R E\' R\', U2]]", "[R E\' R\', U\']", "[U\' : [U\', R E\' R\']]", "[S : [R E\' R\', U\']]", "[r U L\' : [S\', L2]]", "[L\', D M D\']", "[R : [R E\' R\', U\']]", "[U\' M\' : [U\', R E\' R\']]", "[E, L\' U L]", "[l U : [M\', U2]]", "[u R : [S, R2]]", "[M\' U R : [E\', R2]]", "[E R U\' R\' : [E, R2]]", "[U\' L U, M\']", "[L\' : (M D M\' D)2]", "[E\', R\' U\' R]", "[F\' : [L2, E\']]", "[u\' R\' : [E, R2]]", "[M : [U\' L U, M2]]", "[u L\' : [E, L2]]", "[R\' U\' : [R2, S]]", "[E\' : [R\' E R, U\']]", "[L\' E2 L, U]", "[S\' U L : [E\', L2]]", "[R U\' R\', E]", "[L\' E\' : [E\', L U L\']]", "[R : [U\', R E\' R\']]", "[M\', U\' R U]", "[R\' U\' R : [E, R2]]", "[l F : [l\' S\' l, F]]", "[R U\' R\', E\']", "[R\' f R\' : [S\', R2]]", "[M, U\' R U]", "[R\' U\' R\' : [E\', R2]]", "[U\' : [U\' M U, R]]", "[U R\' : [S, R2]]", "[D\' U L : [E\', L2]]", "U R\' U\' R\' U R U R U R\' U2", "[U D L : [E\', L2]]", "[U L : [E\', L2]]", "[U M U : [M, U2]]", "[R U\' R\' U, M\']", "[L\' U L U\', M\']", "[L F : [E\', L\' U L]]", "[M\' : [U\' L\' U, M\']]", "[U : [S, L F L\']]", "[U\' M\' : [R E\' R\', U\']]", "[M\', U\' R U]", "[U L\' U\', M\']", "[r : [U R\' U\', M\']]", "[M\' : [U R\' U\', M\']]", "[U\' : [S\', R\' F\' R]]", "[M\' : [U R U\', M\']]", "[U\' : [S, R\' F\' R]]", "[U L U\', M\']", "U M\' U\' M U2 M U M\' U", "[U\' R\' U, M\']", "[U\' R2 U, M\']", "[D : [R F R\', S\']]", "[U\' L F L : [S\', L2]]", "[U : [R E2 R\', U2]]", "[R E2 R\', U\']", "[E : [L E\' L\', U]]", "[r\' F\' : [r S r\', F\']]", "[S\' U\' R\' : [E, R2]]", "[L\' U L, E]", "[R\' U\' R\' : [E, R2]]", "[M\', U L\' U\']", "[S U\' R\' : [E, R2]]", "[L\' : [U, L\' E L]]", "[R E : [E, R\' U\' R]]", "[L\' U L, E\']", "[M, U L\' U\']", "[U\' L : [S\', L2]]", "[U : [U M U\', L\']]", "[R U\' R\' : [E, R2]]", "[U\' D R\' : [E, R2]]", "[U\' R\' : [E, R2]]", "[U\' D\' R\' : [E, R2]]", "U\' L U L U\' L\' U\' L\' U\' L U2", "[S U\' : [R2, S]]", "[R\' F R, S]", "[M U\' : [M\', U2]]", "[M L\' U\' : [M\', U2]]", "[L F\' L\' : [S\', L2]]", "[l U\' : [M\', U2]]", "[l F : [F, l\' S\' l]]", "[R\' F\' : [R U\' R\', E]]", "[S U\' R : [E, R2]]", "[S\' : [U, L\' E L]]", "[F\' R : [E, R2]]", "[S\' : [U, L E\' L\']]", "[R U R\' U\', M\']", "[S U\' R\' : [E\', R2]]", "[M\' : (U\' M\' U\' M)2]", "[R u\' R : [E\', R2]]", "[M : u M u2 M u]", "[R\' F R, S\']", "[r : [M2, U R\' U\']]", "[R\' : [F, R\' S\' R]]", "[U\' : [L\' E L, U2]]", "[U : [U, L\' E L]]", "[L\' E L, U]", "[r\' U\' : [M\', U2]]", "[r U R : [E, R2]]", "[L F\' : [L2, E]]", "[u R\' : [S, R2]]", "[E\', R U\' R\']", "[D R D\', M]", "[L\' : [L\' E L, U]]", "[S\' : [L\' E L, U]]", "[R, D\' M D]", "[E R U\' R : [E\', R2]]", "[U R\' U\', M\']", "[E, L U L\']", "[R : (M D\' M\' D\')2]", "[F : [R2, E]]", "[u\' R : [E\', R2]]", "[M : [U R\' U\', M2]]", "[u L : [E\', L2]]", "[U : [R E R\', U2]]", "[R E R\', U\']", "[U\' : [U\', R E R\']]", "[R\' F R\' : [S, R2]]", "[M\' U R : [E, R2]]", "[M\' D\' : [M\', D2]]", "[M\' U R\' : [E\', R2]]", "[S U L\' : [E\', L2]]", "[U\' : [R\' F\' R, S\']]", "[R E : [R\' U\' R, E]]", "[F\' R\' : [E, R2]]", "[D\' M D, R]", "[D\' M D, R\']", "[U R2 U\', M\']", "[R\' E\' : [R U\' R\', E\']]", "(M D\' M\' D\')2", "[S U L : [E, L2]]", "[R\' F : [R2, E]]", "[U\' : [S, R\' B R]]", "[R F R\', S\']", "[U\' : [L E\' L\', U2]]", "[U : [U, L E\' L\']]", "[L E\' L\', U]", "[M R U\' : [M\', U2]]", "[U\' E\' L : [S\', L2]]", "[M\' U\' L : [E\', L2]]", "[E R U\' R : [E, R2]]", "[F\' : [R U\' R\', E]]", "[E\', L\' U L]", "[S\' : [L E\' L\', U]]", "[l\' U\' R\' : [S, R2]]", "[R\', D\' M D]", "[U R U\', M\']", "[L : [L E\' L\', U]]", "[R\' : (M D\' M\' D\')2]", "[E, R\' U\' R]", "[R2\' F : [R2, E]]", "[U\' E\' R\' : [E, R2]]", "[M : [U R U\', M2]]", "[U E L\' : [E, L2]]", "[R\' U\' R U, M]", "[L U L\' U\', M]", "[M\', L\' U\' L U]", "[M\', U\' L\' U]", "[M\', U\' L2 U]", "[M\', U\' L U]", "[U\' R U, M]", "[U\' : [R\' F\' R, S]]", "[U L\' U\', M]", "[M\', R U R\' U\']", "[M\', U R\' U\']", "[M\', U R2 U\']", "[M\', U R U\']", "[U L U\', M]", "[D\' : [M\', U R2 U\']]", "[U\' R\' U, M]", "[U : [S, R\' F\' R]]", "[R\' : [U\' R\' U, M]]", "[U : [S, R B R\']]", "[U D : [S, R\' F\' R]]", "[U : [R\' E2 R, U2]]", "[R\' E2 R, U\']", "[E\' : [L\' E L, U]]", "[L\' u L : [E, L2]]", "[L U L\', E\']", "[S\' U\' R : [E\', R2]]", "[R\' U\' R : [E\', R2]]", "[M\', U L U\']", "[U\' L\' : [S\', L2]]", "[S U\' R : [E\', R2]]", "[L U L\', E]", "[R\' E\' : [E\', R U\' R\']]", "[L : [U, L E\' L\']]", "[M, U L U\']", "[U : [U M U\', L]]", "[R U\' R : [E\', R2]]", "[U\' D R : [E\', R2]]", "[U\' R : [E\', R2]]", "[U\' D\' R : [E\', R2]]", "U\' L\' U\' L\' U L U L U L\'", "[U\' M\' U : [M\', U2]]", "(M U\' M\' U\')2", "(M U M\' U)2", "[M\' : (M\' U\' M U\')2]", "[L : (D\' M D\' M\')2]", "(D\' M D\' M\')2", "[L\' : (D\' M D\' M\')2]", "[U\' : [R, U\' M U]]", "U M U M\' U2 M\' U\' M U", "[U : [L\', U M U\']]", "[M\' : (M\' U M U)2]", "[R : (D M D M\')2]", "(D M D M\')2", "[r : (M\' U M U)2]", "[D\' : [U R2 U\', M\']]", "[U : [L, U M U\']]", "[U\' : [R\', U\' M U]]", "[D\' : [R F R\', S\']]", "[D\' R\' F : [R2, E]]", "[D L F\' : [L2, E\']]", "[R U\' : [R2, S]]", "[E : [R E\' R\', U\']]", "[L E2 L\', U]", "[S\' U L\' : [E, L2]]", "[R\' : [U\', R\' E R]]", "[L E : [E, L\' U L]]", "[R\' U\' R, E\']", "[U R : [S, R2]]", "[M\', U\' R\' U]", "[R U\' R : [E, R2]]", "[R u\' R\' : [E\', R2]]", "[S U L\' : [E, L2]]", "[R\' U\' R, E]", "[M, U\' R\' U]", "[R U\' R\' : [E\', R2]]", "[U\' : [U\' M U, R\']]", "[U D\' L\' : [E, L2]]", "U R U R U\' R\' U\' R\' U\' R", "[U D L\' : [E, L2]]", "[U L\' : [E, L2]]", "[M\', U2]", "[U, M U2 M]", "[R\' F : [R S R\', F2]]", "[M : u M\' u2 M\' u]", "[L2 F\' : [E\', L2]]", "[L F\' : [E\', L2]]", "[F\' : [E\', L2]]", "[U D\' L\' : [E\', L2]]", "[U\' D R : [E, R2]]", "[M : u\' M\' u2 M\' u\']", "[F : [E, R2]]", "[R\' F : [E, R2]]", "[R2\' F : [E, R2]]", "[U : [R\' F\' R, S]]", "[U\' D R\' : [E\', R2]]", "[D\' : [S\', R F R\']]", "[U D\' L : [E, L2]]", "[R\' F : [R S\' R\', F2]]", "u2 M\' u2 M\'", "[R\' F : [R\' S\' R, F2]]", "[U\' : [R2, S]]", "[R U R\' : [S, R2]]", "R\' U\' R U R U R U\' R\' U\'", "[L : [L S L\', F\']]", "[U\' E\' R\' : [E, R2]]", "[S, L\' F\' L]", "[u\' R : [E, R2]]", "U2 R U\' R\' U\' R\' U\' R U R U\'", "[U R\' F\' R : [S, R2]]", "[U\' R : [E, R2]]", "[S\', R\' F R]", "[u\' R\' : [E\', R2]]", "[U\' E\' R : [E, R2]]", "[r\' : [U\' R\' U, M\']]", "[U\' R\' : [E\', R2]]", "[D\' R\' F : [E, R2]]", "R\' U R U R U R\' U\' R\' U\'", "[R\' F\' : [R S\' R\', F2]]", "[M2, U\' R2 U]", "[D : [M, D2]]", "[U2, M]", "[R U\' R\' U, M2]", "D\' R\' U\' R U R U R U\' R\' U\' D", "[D : [S, L F\' L\']]", "[M\' : [U\' L\' U, M2]]", "[U : [L B\' L\', S\']]", "[M\' : [U\' L U, M2]]", "[D U L\' : [E\', L2]]", "[D : [S\', R F R\']]", "[D\' U\' R : [E, R2]]", "[r : [U R\' U\', M2]]", "[M\' : [U R\' U\', M2]]", "[U\' : [R\' B R, S]]", "[M\' : [U R U\', M2]]", "[D\' r\' : [U\' R\' U, M\']]", "[U\' D\' R\' : [E\', R2]]", "[U D L : [E, L2]]", "M u2 M u2", "[U\' R2 U, M2]", "[L\' D\' U\' R : [E, R2]]", "[U : [L2, S\']]", "L U L\' U\' L\' U\' L\' U L U", "[L\' U\' L : [S\', L2]]", "[S, L F\' L\']", "[U E L\' : [E\', L2]]", "[u L : [E, L2]]", "[U L\' : [E\', L2]]", "[U\' L F L\' : [S\', L2]]", "U2 L\' U L U L U L\' U\' L\' U", "[R\' : [R\' S\' R, F]]", "[u L\' : [E\', L2]]", "[S\', R F R\']", "[U E L\' : [E, L2]]", "[U D : [R\' F\' R, S]]", "L U\' L\' U\' L\' U\' L U L U", "[D L F\' : [E\', L2]]", "[U L : [E, L2]]", "[R\' F\' : [R\' S\' R, F2]]", "[D\' : [M, D2]]", "[L\' D\' U\' R\' : [E, R2]]"]
 
+var letterpairs = []
+var allpairs = []
+
+//alphabet is positions
+var alphabet = ["UB", "UR", "UL", "LU", "LF", "LD", "LB", "FR", "FD", "FL", "RU", "RB", "RD", "RF", "BU", "BL", "BD", "BR", "DF", "DR", "DB", "DL"];
+
+alphabet.forEach(prompt);
+alphabet.forEach(buttons);
+alphabet.forEach(createpairs);
+
+function prompt (place) {
+		var y = document.createElement("INPUT")
+		y.setAttribute("type", "text")
+		y.setAttribute("id", place + "x")
+		y.setAttribute("value", place)
+		y.setAttribute("class", "checkboxFalse")
+		var father = document.getElementById("prompted")
+		father.appendChild(y);
+}
+
+function buttons(value) {
+		//make buttons
+		var x = document.createElement("INPUT")
+		x.setAttribute("type", "button")
+		x.setAttribute("id", value)
+		x.setAttribute("onclick", "Select" + "(" + "value" + ")")
+		x.setAttribute("value", value)
+		x.setAttribute("class", "checkboxFalse")
+		var parent = document.getElementById("checkboxes")
+		parent.appendChild(x);
+}
+
+function createpairs(value) {
+        //make letter pairs
+        letterpairs = []
+        if (allpairs.length == 440) {
+        allpairs = []
+        }
+        alphabet.forEach(pairs)
+        function pairs(letter) {
+        var pair = value + letter
+        if (value != letter && pair != "UBBU" && pair != "URRU" && pair != "ULLU" && pair != "LUUL" && pair != "LFFL" && pair != "LDDL" && pair != "LBBL" && pair != "FRRF" && pair != "FDDF" && pair != "FLLF" && pair != "RUUR" && pair != "RBBR" && pair != "RDDR" && pair != "RFFR" && pair != "BUUB" && pair != "BLLB" && pair != "BDDB" && pair != "BRRB" && pair != "DFFD" && pair != "DRRD" && pair != "DBBD" && pair != "DLLD"){
+        var TOne = document.getElementById(value + "x").value
+        var TTwo = document.getElementById(letter + "x").value
+        pair = TOne + TTwo
+        allpairs.push(pair)
+        }
+    	}
+        
+}
+
+// selectSet
+function Select(p) {
+var y = document.getElementById(p)
+if (y.className == "checkboxFalse") {
+	y.className = "checkboxTrue"
+var i;
+var j;
+	for (i = 0; i < alphabet.length; i++) {
+    	if (alphabet[i] == p) {
+    		for (j = (20 * i); j < (20 * i + 20); j++) {
+            letterpairs.push(allpairs[j])
+			}
+    	} 
+    }
+} 
+else {
+	y.className = "checkboxFalse"
+var k;
+	for(var k = letterpairs.length - 1; k >= 0; k--) {
+    	if(letterpairs[k].charAt(0) == document.getElementById(p + "x").value) {
+       		letterpairs.splice(k, 1);
+    	}
+	}
+}
+}
+
+//get random pairs
+function showpair() {
+document.getElementById("case").innerHTML = letterpairs[Math.floor(Math.random() * letterpairs.length)]
+  
+	if (letterpairs.length == 0) {
+		document.getElementById("case").innerHTML = "Select a set"
+	}
+}
+
+//generate the correct comm
+var ComNow;
+function getcom() {
+ var i;
+	for (i = 0; i < allpairs.length; i++) {
+		if (document.getElementById("case").innerHTML == allpairs[i]) {
+    	ComNow = comms[i]
+        }
+     if (letterpairs.length == 0) {
+		document.getElementById("comm").innerHTML = "Select a set"
+	}   
+    }
+}    
+
+//always show alg
+var Yesorno = 0;
+function yesorno() {
+	if (Yesorno == 0) {
+		Yesorno += 1
+    	document.getElementById("yesorno").value = "Never show algorithm"    
+	}
+	else {
+		Yesorno = 0
+		document.getElementById("yesorno").value = "Always show algorithm"    
+	}
+}
+
+//add everything together
+document.onkeydown = function(event) { 
+	if (event.keyCode == 32) {
+		document.getElementById("comm").innerHTML = "Press enter to show the alg"
+		showpair(); 
+		getcom();
+		if (Yesorno == 0 && letterpairs.length != 0) {
+			document.getElementById("comm").innerHTML = ComNow;
+		}
+        if (document.getElementById("case").innerHTML.length > 2 && document.getElementById("case").innerHTML != "Select a set") {
+        document.getElementById("case").innerHTML = "Make a correct letterscheme"
+        document.getElementById("comm").innerHTML = "letterscheme"
+        }
+	}
+	if (event.keyCode == 13 && letterpairs.length != 0) {
+	document.getElementById("comm").innerHTML = ComNow;
+	}
+}
